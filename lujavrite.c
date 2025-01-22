d765b2bf25414867735abbf4d509744ea0f4ee9a
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-01-22 09:12:03 +0100

    PoC for calling Lua code from Java

--- lujavrite.c
+++ lujavrite.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2023 Red Hat, Inc.
+ * Copyright (c) 2023-2025 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define __USE_GNU 1
@@ -25,6 +26,7 @@
 #include <lauxlib.h>
 
 static JNIEnv *J;
+static __thread lua_State *LL;
 
 /**
  * Determine whether embedded Java Virtual Machine has already been initialized.
@@ -155,7 +157,13 @@ call(lua_State *L)
     }
   }
 
+  assert(LL == NULL);
+  LL = L;
+
   jstring ret = (*J)->CallStaticObjectMethodA(J, jcls, methodId, args);
+
+  LL = NULL;
+
   if ((*J)->ExceptionCheck(J)) {
     (*J)->ExceptionDescribe(J);
     exit(66);
@@ -190,4 +198,81 @@ luaopen_lujavrite(lua_State *L)
   lua_newtable(L);
   luaL_setfuncs(L, functs, 0);
   return 1;
+}
+
+
+static void check_lua_state(JNIEnv *J)
+{
+  (void)J;
+  if (LL == NULL) {
+    fprintf(stderr, "lujavrite: unable to call Lua from Java: Lua state is NULL\n");
+    exit(66);
+  }
+}
+
+JNIEXPORT jint JNICALL
+Java_io_kojan_lujavrite_Lua_getglobal(JNIEnv *J, jobject this, jstring name)
+{
+  (void)this;
+  check_lua_state(J);
+  const char *name1 = (*J)->GetStringUTFChars(J, name, NULL);
+  jint ret = lua_getglobal(LL, name1);
+  (*J)->ReleaseStringUTFChars(J, name, name1);
+  return ret;
+}
+
+JNIEXPORT jint JNICALL
+Java_io_kojan_lujavrite_Lua_getfield(JNIEnv *J, jobject this, jint index, jstring name)
+{
+  (void)this;
+  check_lua_state(J);
+  const char *name1 = (*J)->GetStringUTFChars(J, name, NULL);
+  jint ret = lua_getfield(LL, index, name1);
+  (*J)->ReleaseStringUTFChars(J, name, name1);
+  return ret;
+}
+
+JNIEXPORT void JNICALL
+Java_io_kojan_lujavrite_Lua_pushstring(JNIEnv *J, jobject this, jstring string)
+{
+  (void)this;
+  check_lua_state(J);
+  const char *string1 = (*J)->GetStringUTFChars(J, string, NULL);
+  (void)lua_pushstring(LL, string1);
+  (*J)->ReleaseStringUTFChars(J, string, string1);
+}
+
+JNIEXPORT jint JNICALL
+Java_io_kojan_lujavrite_Lua_pcall(JNIEnv *J, jobject this, jint nargs, jint nresults, jint msgh)
+{
+  (void)this;
+  check_lua_state(J);
+  jint ret = lua_pcall(LL, nargs, nresults, msgh);
+  return ret;
+}
+
+JNIEXPORT jstring JNICALL
+Java_io_kojan_lujavrite_Lua_tostring(JNIEnv *J, jobject this, jint index)
+{
+  (void)this;
+  check_lua_state(J);
+  const char *val1 =  lua_tostring(LL, index);
+  jstring val = (*J)->NewStringUTF(J, val1);
+  return val;
+}
+
+JNIEXPORT void JNICALL
+Java_io_kojan_lujavrite_Lua_remove(JNIEnv *J, jobject this, jint index)
+{
+  (void)this;
+  check_lua_state(J);
+  lua_remove(LL, index);
+}
+
+JNIEXPORT void JNICALL
+Java_io_kojan_lujavrite_Lua_pop(JNIEnv *J, jobject this, jint n)
+{
+  (void)this;
+  check_lua_state(J);
+  lua_pop(LL, n);
 }
